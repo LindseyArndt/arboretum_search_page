2e271e1298768b8a4865b3290a8b8f30b34c17b0
Ahlea Wright <wrightahlea@gmail.com>
2023-04-27 12:50:47 -0500

    Update kwiz.js
    description of results updated

--- kwiz.js
+++ kwiz.js
@@ -1,28 +1,28 @@
 function mostlyA() {
   var x = document.getElementById("myDIV");
   if (x.innerHTML === "") {
-    x.innerHTML = "You are a Pagoda Dogwood! Your healing nature and bold personality make you a favorite among friends, and your versatility allows you to enjoy a bunch of different activites. Check out the Search page to find out more information about your tree!";
+    x.innerHTML = "You are a Pagoda Dogwood! Your healing nature and kind personality make you a favorite among friends, and your versatility allows you to enjoy a bunch of different activites. Check out the Search page to find out more information about your tree!";
    }
 }
 
 function mostlyB() {
   var x = document.getElementById("myDIV");
   if (x.innerHTML === "") {
-    x.innerHTML = "You are a Merrill Magnolia! Like this tree, you have a Check out the Tree Search page to find out more information about your tree!";
+    x.innerHTML = "You are a Merrill Magnolia! Like this tree, you have a bold personality that demands attention from observers. However, there are times when you need a little TLC - don't forget to moniter how you're doing and practive self care. Check out the Tree Search page to find out more information about your tree!";
   }
 }
 
 function mostlyC() {
   var x = document.getElementById("myDIV");
   if (x.innerHTML === "") {
-    x.innerHTML = "You are a Sugar Maple! Check out the Tree Search page to find out more information about your tree!";
+    x.innerHTML = "You are a Sugar Maple! You are passionate and are fierce, and your friends and family know that your loyalty and support does not come without a somewhat prickly personality. However, they know you have their backCheck out the Tree Search page to find out more information about your tree!";
   }
 }
 
 function mostlyD() {
   var x = document.getElementById("myDIV");
   if (x.innerHTML === "") {
-    x.innerHTML = "You are a Red Pine! Check out the Tree Search page to find out more information about your tree!";
+    x.innerHTML = "You are a Red Pine! You are hardy and dependable. You are known for having the best ideas, and you are also great at delegating tasks. Check out the Tree Search page to find out more information about your tree!";
   }
 }
 
